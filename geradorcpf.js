4a765165a50ac721d33ef09f78243bdb68fc7596
Daniel Ferreira <danenoisturbera@gmail.com>
2017-03-24 13:35:08 -0300

    Update geradorcpf.js

--- geradorcpf.js
+++ geradorcpf.js
@@ -1,21 +1,21 @@
 window.onload = function () {
     function exec(){
-		var arr = [], mult = [10,9,8,7,6,5,4,3,2], res = [], soma = 0, resto = 0, div = 0, dig1 = 0, dig2 = 0;
+		var arr = [],
+			mult = [10,9,8,7,6,5,4,3,2],
+			res = [],
+			soma = 0,
+			resto = 0,
+			div = 0,
+			dig1 = 0,
+			dig2 = 0;
 		
-	    for (var i = 0; i < 9; i++) {
-	        arr[i] = getRandomInt(0, 9);
-	    }
+	   
+		arr = retornarSequenciaAleatoria();
+		res = multiplicar(9, arr, mult);
+		soma = somar(9, res);	   
 
-	    for (var i = 0; i < 9; i++) {
-	        res.push(arr[i] * mult[i]);
-	    }
-
-	    for (var i = 0; i < 9; i++) {
-	        soma += res[i];
-	    }
-
-	    resto = soma % 11;
-	    div = Math.floor(soma / 11);
+	    resto = restoModPorOnze(soma);
+	    div = dividirPorOnze(soma);
 	    dig1 = validar(resto, div);
 	    soma = 0;
 	  	res = [];
@@ -23,43 +23,72 @@ window.onload = function () {
 		arr.push(dig1);
 		mult = [11,10,9,8,7,6,5,4,3,2];
 		
-	    for (var i = 0; i < 10; i++) {        
-	        res.push( arr[i] * mult[i]);
-	    }
+		res = multiplicar(10, arr, mult);
+		soma = somar(10, res);	
 		
-		for (var i = 0; i < 10; i++) {
-	        soma += res[i];
-	    }
-		
-		resto = soma % 11;
-	    div = Math.floor(soma / 11);
+		resto = restoModPorOnze(soma);
+	    div = dividirPorOnze(soma);
 	    dig2 = validar(resto, div);
 	    
 	    arr.push(dig2);
 	    var str = "";
 
-	    for (var i = 0; i < 11; i++) {
+	    for (i = 0; i < 11; i++) {
 	        str += arr[i].toString();
 	    }
 
 	    document.writeln(str);
-
-	    function getRandomInt(min,max)
-	    {
-	        return Math.floor(Math.random() * (max - min) + min);
-	    }
-
-	    function validar(resto, div)
-	    {
-	        if (resto < 2) {
-	            return 0;
-	        }
-
-	        return 11 - resto;
-	    }
-		
-	}
+	}		
 	
 	exec();
 	
-}
+	function retornarSequenciaAleatoria()
+	{
+		var arrayInt = [];
+		 for (var i = 0; i < 9; i++) {
+	        arrayInt[i] = retornarIntAleatorio(0, 9);
+	    }
+		
+		 return arrayInt;
+	}
+	
+	function retornarIntAleatorio(min,max){  
+        return Math.floor(Math.random() * (max - min) + min);
+    }	
+	
+	function validar(resto, div){
+	    if (resto < 2) {
+	        return 0;
+	    }
+
+	    return 11 - resto;
+	}
+
+	function multiplicar(tamanho, arr, mult){
+		var res = [];
+		for (var i = 0; i < tamanho; i++){
+			res.push(arr[i] * mult[i]);
+		}
+
+		return res;
+	}
+
+	function somar(tamanho, res){
+		var soma = 0;
+
+		for (i = 0; i < tamanho; i++) {
+	        soma += res[i];
+	    }
+
+		return soma;
+	}
+
+	function dividirPorOnze(soma){
+		return Math.floor(soma / 11);
+	}
+	
+	function restoModPorOnze(soma){
+		return soma % 11;
+	}
+	
+};
